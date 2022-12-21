ab2f510701da35af7f73c8f53fd9b95ec9efba30
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-12-21 13:10:04 -0500

    assets added

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import Home from './screens/Home';
+import Details from './screens/Details';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
+const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf")
+  })
   return (
-    <View style={styles.container}>
-      <Text>thought for me whole world!</Text>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+

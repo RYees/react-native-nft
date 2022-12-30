651ff5b6365b1ea9426fef4b99740944f3369124
Rehmet Yeshanew <ryfgmz87@gmail.com>
2022-12-30 14:46:57 -0500

    details component

--- screens/Details.js
+++ screens/Details.js
@@ -1,8 +1,20 @@
-import { Text } from "react-native"
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native"
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants"
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar,
+DetailDesc, DetailsBid } from "../components"
 
-const Details = () => {
+const Details = ({route, navigation}) => {
+  const { data } = route.params;
   return (
-    <Text>Details</Text>
+    
+    <SafeAreaView style={{flex: 1}}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+     <Text>new</Text>
+    </SafeAreaView>
   )
 }
 
